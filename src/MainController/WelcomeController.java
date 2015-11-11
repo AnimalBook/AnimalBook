32d59d40fe244ba499ef9d8bbd0c3a5b1b3181db
Soklundy <bonglundy208@gmail.com>
2015-11-11 17:04:38 +0700

    Config Full

---
+++ src/MainController/WelcomeController.java
@@ -0,0 +1,18 @@
+package MainController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView welcomePage(){
+		ModelAndView model = new ModelAndView("welcomePage");
+		model.addObject("message", "welcomePage");
+		return model;
+	}
+	
+	
+}
